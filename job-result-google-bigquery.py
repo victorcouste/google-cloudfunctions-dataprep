7d84e09ea26820fcf4e2fce55891b1cff959ca1e
Victor Coustenoble <victor.coustenoble@gmail.com>
2020-11-24 09:46:02 +0100

    Update job-result-google-bigquery.py

--- job-result-google-bigquery.py
+++ job-result-google-bigquery.py
@@ -39,11 +39,11 @@ def publish_bigquery(request):
    createdAt = job_object["jobs"]["data"][0]["createdAt"]
 
     if "cpJobId" in job_object["jobs"]["data"][0]:
-	    dataflow_jobid = job_object["jobs"]["data"][0]["cpJobId"]
+	dataflow_jobid = job_object["jobs"]["data"][0]["cpJobId"]
         updatedAt = job_object["jobs"]["data"][1]["updatedAt"]
     else:
         # there is an ingest or convert job before the dataflow transform job
-	    dataflow_jobid = job_object["jobs"]["data"][1]["cpJobId"]
+	dataflow_jobid = job_object["jobs"]["data"][1]["cpJobId"]
         updatedAt = job_object["jobs"]["data"][2]["updatedAt"]
 
     print('Dataflow jobId : {}'.format(dataflow_jobid))
